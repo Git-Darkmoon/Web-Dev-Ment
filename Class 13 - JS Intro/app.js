11032f6e6835317b0fd505755e73a724b172e9a6
Nicolas Mayorga <namayorgav@gmail.com>
2023-07-21 09:32:04 -0500

    JS Intro.

---
+++ Class 13 - JS Intro/app.js
@@ -0,0 +1,60 @@
+console.log("Hello world")
+
+// "Variables"
+
+// var (NO USARLA), let, const
+
+const NOMBRE = "Victor"
+let age = 19
+let actualYear = 2023
+
+// console.log("La f de nacimiento de " + NOMBRE + " es: " + (actualYear - age))
+
+// template strings (` `) ( Spanish keyboard => altGr + ]} )
+
+let message = `${NOMBRE}'s Birth year: ${actualYear - age}`
+
+// console.log(message)
+
+// let name = prompt("Enter your name: ")
+// console.log(`hello ${name}`)
+// name = "No tiene nombre"
+// console.log(`Hello ${name}`)
+
+let num1 = 5
+// let augment = prompt("Enter the increase amount: ") /* prompt returns string */
+// let augmentValue = parseInt(augment)
+// let result = num1 + augmentValue
+// console.log(result)
+
+// NaN => Not a Number
+
+// result = result + 1 ==> result += 1 | result++
+
+// result ** 2
+// console.log(result)
+
+// Data types:
+
+/* 
+Primitives:
+- null
+- undefined
+- NaN
+- String
+- Int ?
+- Float ?
+- Boolean
+- number
+- symbol
+
+Non-Primitives:
+- Array
+- Object
+- Map
+- Symbol => object
+*/
+
+// let orange = null
+
+// console.log(orange)
---
+++ Class 13 - JS Intro/nonPrimitive.js
@@ -0,0 +1,14 @@
+// Array:
+let students = ["Vic", "El pepe", "Bapoprub"]
+console.log(students)
+console.log(students[1])
+
+// Objects:
+let student = {
+  name: "Pepito",
+  age: 43,
+  gender: "Binary",
+}
+
+console.log(student)
+console.log(`${student.name} is ${student.age}`)
