0f61d347e970a2bba3d0fc1dc9cab1a0276aa9d6
Nicolas Mayorga <namayorgav@gmail.com>
2023-08-09 21:28:44 -0500

    btn functions.

--- Class 21 - Navbar/app.js
+++ Class 21 - Navbar/app.js
@@ -3,10 +3,11 @@ const closeSidebarBtn = document.getElementById("closeSidebarBtn")
 
 const sideBar = document.querySelector(".sidebar")
 
+// Open btn
 openSidebarBtn.addEventListener("click", () => {
   sideBar.classList.add("openSidebar")
 })
-
+// Close btn
 closeSidebarBtn.addEventListener("click", () => {
   sideBar.classList.remove("openSidebar")
 })
